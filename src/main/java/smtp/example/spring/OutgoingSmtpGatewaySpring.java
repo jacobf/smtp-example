02b6e25925310da6bda0884566778323ae019910
jfahrenkrug <Jacob.Fahrenkrug@epost-dev.de>
2012-11-15 00:07:11 +0100

    string put on a publish-subscribe-channel and then send via smtp with spring-integration

--- src/main/java/smtp/example/spring/OutgoingSmtpGatewaySpring.java
+++ src/main/java/smtp/example/spring/OutgoingSmtpGatewaySpring.java
@@ -1,11 +1,27 @@
 package smtp.example.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.mail.MailHeaders;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+
+import java.util.Properties;
 
 @Component
 public class OutgoingSmtpGatewaySpring {
 
+    @Autowired
+    private MessageChannel outboundMail;
+
+    @Autowired
+    private Properties smtpProperties;
+
     public void send(String s) {
-        System.out.println("spring works correctly");
+        outboundMail.send(MessageBuilder.withPayload(s).
+                setHeader(MailHeaders.TO,smtpProperties.getProperty("mail.to"))
+                        .setHeader(MailHeaders.FROM, smtpProperties.getProperty("mail.from"))
+                        .setHeader(MailHeaders.SUBJECT, "spring-integration works as well")
+                        .build());
     }
 }
